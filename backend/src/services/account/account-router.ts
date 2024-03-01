ae5d918047e2e67688aed030db00d12dbac52e5a
Akul Sharma <akulsharma02@gmail.com>
2024-03-01 14:06:31 -0600

    added account/tickets/test endpoint for frontend dev

--- backend/src/services/account/account-router.ts
+++ backend/src/services/account/account-router.ts
@@ -41,4 +41,38 @@ accountRouter.get("/tickets", async (req: Request, res: Response, next: NextFunc
     return res.status(StatusCode.SuccessOK).json({ success: true, tickets: tickets });
 });
 
+accountRouter.get("/tickets/test", async (req: Request, res: Response, next: NextFunction) => {
+    const profileId = req.query.id as string | undefined;
+
+    if (!profileId) {
+        return next(new RouterError(StatusCode.ClientErrorBadRequest, "profile id query parameter required"));
+    }
+
+    const tickets = await prisma.ticket.findMany({
+        where: { owner_id: Number(profileId) },
+    });
+
+    return res.status(StatusCode.SuccessOK).json({
+        success: true,
+        tickets: [
+            {
+                owner_id: Number(profileId),
+                event_id: 2,
+                used: false,
+                listed: false,
+                created_at: "2024-02-23T03:50:49.086Z",
+            },
+            {
+                owner_id: Number(profileId),
+                event_id: 3,
+                used: false,
+                listed: false,
+                created_at: "2024-02-23T03:50:49.086Z",
+            },
+        ],
+    });
+});
+
+accountRouter.post("/create", async (req: Request, res: Response, next: NextFunction) => {});
+
 export default accountRouter;
