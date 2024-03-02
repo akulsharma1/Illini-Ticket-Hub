9d49426e9a17aa4f761820a46f023d46c3bd0511
Adrian Du <writingclass107@gmail.com>
2024-03-01 18:29:55 -0600

    Added the accountpage & routing from accountButton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Sidebar from "./components/sidebar/sidebar";
 import AccountButton from "./components/accountButton/accountButton";
 import DashboardTickets from "./components/dashboardTickets/dashboardTickets";
+import AccountView from "./components/accountView/accountView";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Dashboard: React.FC = () => {
@@ -18,11 +19,25 @@ const Dashboard: React.FC = () => {
   );
 };
 
+const AccountPage: React.FC = () => {
+  const [activeTab, setActiveTab] = useState("");
+
+  //const handleAccountButtonClick = () => {};
+
+  return (
+    <div className="main-app">
+      <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
+      <AccountView />
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/account" element={<AccountPage />} />
       </Routes>
     </Router>
   );
