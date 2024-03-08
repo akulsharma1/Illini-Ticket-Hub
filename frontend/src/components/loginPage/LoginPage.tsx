38cd1f4a7887ed0ccda55189d5656ab7c0ad242a
naynoraa <aaron.t.yan@gmail.com>
2024-03-07 22:14:16 -0600

    Implemented backend linking with database for login page and stored profile data in context

--- frontend/src/components/loginPage/LoginPage.tsx
+++ frontend/src/components/loginPage/LoginPage.tsx
@@ -1,25 +1,46 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom'; // Import Link component for routing
-import './LoginPage.css'; // Import CSS file for styling
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom"; // Import Link component for routing
+import "./LoginPage.css"; // Import CSS file for styling
+import {
+  UserProfileProvider,
+  useUserProfile,
+} from "../userProfile/userProfile";
 
 const LoginPage: React.FC = () => {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const { setUserProfile } = useUserProfile();
   const navigate = useNavigate();
 
-  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
   };
 
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
 
-  const handleLogin = () => {
-    // TODO - add backend logging in logic here
-    // Need to add error handling here, by searching in database and giving an error if not found
-    const userData = { username };
-    navigate("/dashboard", {state: { userData } }); // go to dashboard after someone logs in
+  const handleLogin = async () => {
+    try {
+      const response = await fetch("http://localhost:5555/account/sign-in", { // fetch email and password data from backend
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email_address: email,
+          password: password,
+        }),
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        console.error("Login failed:", data.error);
+      }
+      setUserProfile(data.profile);
+      navigate("/dashboard", { state: { userData: data } });
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
   };
 
   return (
@@ -34,12 +55,12 @@ const LoginPage: React.FC = () => {
         <div className="card-body">
           <form>
             <div className="form-group">
-              <label htmlFor="username">Username:</label>
+              <label htmlFor="email">Email:</label>
               <input
                 type="text"
-                id="username"
-                value={username}
-                onChange={handleUsernameChange}
+                id="email"
+                value={email}
+                onChange={handleEmailChange}
               />
             </div>
             <div className="form-group">
@@ -51,9 +72,17 @@ const LoginPage: React.FC = () => {
                 onChange={handlePasswordChange}
               />
             </div>
-            <button type="button" className="login-button" onClick={handleLogin}>Login</button>
+            <button
+              type="button"
+              className="login-button"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
             <div className="create-account">
-              <p>New? <Link to="/create-account">Create an Account</Link></p>
+              <p>
+                New? <Link to="/create-account">Create an Account</Link>
+              </p>
             </div>
           </form>
         </div>
@@ -62,4 +91,10 @@ const LoginPage: React.FC = () => {
   );
 };
 
-export default LoginPage;
+const LoginPageDataStored: React.FC = () => ( // Wraps component in context
+  <UserProfileProvider>
+    <LoginPage />
+  </UserProfileProvider>
+);
+
+export default LoginPageDataStored;
