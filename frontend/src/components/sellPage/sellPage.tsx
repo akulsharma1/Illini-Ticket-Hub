cfe6aba9e3c2d4cc3634e22101bf669c8d3c5955
Ritam nandi <nandi.ritamnandi@gmail.com>
2024-03-29 18:08:40 -0500

    Sell page has its own unique pages for each event

--- frontend/src/components/sellPage/sellPage.tsx
+++ frontend/src/components/sellPage/sellPage.tsx
@@ -1,14 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "./sellPage.css";
 
 interface Event {
-  id: number;
-  name: string;
-  lowestAsk: number;
-  highestBid: number;
-}
-
-interface EventInfo {
   event_id: number;
   event_type: string;
   event_start: Date;
@@ -17,34 +11,77 @@ interface EventInfo {
 }
 
 const SellPage: React.FC = () => {
-  const [event, setEvent] = useState<Event>({
-    id: 1,
-    name: "Sell Illinois vs Maryland tickets",
-    lowestAsk: 50,
-    highestBid: 100,
-  });
+  const { eventId } = useParams<{ eventId: string }>();
+  const [event, setEvent] = useState<Event | null>(null);
+  const [lowestAsk, setLowestAsk] = useState<number>(-1);
+  const [highestBid, setHighestBid] = useState<number>(-1);
 
-  const handleSellNow = () => {
-    console.log("Buying now at lowest ask:", event.lowestAsk);
-  };
+  useEffect(() => {
+    const fetchData = async (eventId: number) => {
+      try {
+        // Fetch event details, lowest ask, and highest bid
+        const eventResponse = await fetch(
+          `http://localhost:5555/events/prices/${eventId}`
+        );
+        if (!eventResponse.ok) {
+          throw new Error("Failed to fetch event data");
+        }
+        const eventData = await eventResponse.json();
+        setEvent(eventData.event);
+        setLowestAsk(eventData.lowest_ask);
+        setHighestBid(eventData.highest_bid);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-  const handlePlaceAsk = () => {
-    console.log("Placing new ask");
-  };
+    const storedEvent = localStorage.getItem("currEvent");
+    if (storedEvent) {
+      const storedEventData = JSON.parse(storedEvent);
+      setEvent(storedEventData);
+      // Retrieve event ID from stored event data
+      const eventId = storedEventData.event_id;
+      fetchData(eventId);
+    } else {
+      console.error("Event not found in local storage");
+    }
+  }, []);
+
+  if (!event) {
+    return <div>Event not found</div>;
+  }
+
+  const formattedDate = event.event_start
+    ? new Date(event.event_start).toLocaleDateString()
+    : "";
 
   return (
     <div className="sell-page">
-      <div className="event-card-sell">
-        <div className="card-header">
-          <h1 className="card-title">{event.name}</h1>
+      <div className="container">
+        <div className="event-card-sell">
+          <div className="card-header">
+            <h1 className="card-title">{event.event_type}</h1>
+          </div>
+          <div className="card-content">
+            <p>Away Team: {event.away_team}</p>
+            <p>Stadium: {event.stadium_location}</p>
+            <p>Event Start: {formattedDate}</p>
+          </div>
         </div>
-        <div className="card-content">
+        <div className="action-card">
+          <div className="card-header">
+            <h2 className="card-title">Actions</h2>
+          </div>
           <div className="button-container">
-            <button className="buy-button" onClick={handleSellNow}>
-              Sell Now at Highest Bid: ${event.highestBid}
+            <button className="buy-button">
+              Buy Now
+              <br />
+              Lowest Ask: {lowestAsk}
             </button>
-            <button className="sell-button" onClick={handlePlaceAsk}>
-              Place New Ask (Current Lowest Ask: ${event.lowestAsk})
+            <button className="sell-button">
+              Place New Bid
+              <br />
+              Highest Bid: {highestBid}
             </button>
           </div>
         </div>
