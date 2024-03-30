4de01e76f707c328d41382d8d3215202dbb90467
akulsharma1 <akulsharma02@gmail.com>
2024-03-29 22:07:33 -0500

    removed old routers

--- backend/src/services/marketplace/sell/sell-router.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from "express";
-
-const sellRouter: Router = Router();
-
-sellRouter.get("/test", async (_: Request, res: Response) => {
-    return res.status(200).json({ success: true });
-});
-
-export default sellRouter;
